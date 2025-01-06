9deae8a8e26e43ed2b1206a3fedf18756a150b7b
Nelson Mokoro <67539182+1Nelsonel@users.noreply.github.com>
2025-01-06 09:07:27 +0300

    fix origin issues

--- MiniCrm/settings.py
+++ MiniCrm/settings.py
@@ -83,8 +83,8 @@ MIDDLEWARE = [
 
 
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
-CSRF_TRUSTED_ORIGINS = ['http://localhost:5173']
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173","https://nelsoneltechgroup.co.ke","https://185.113.249.150"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:5173","https://nelsoneltechgroup.co.ke","https://185.113.249.150"]
 
 # Retrieve Redis host from environment variables
 redis_host = env("REDIS_HOST")
